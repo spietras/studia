bd0e1cfa7ba31cd64ec82c962e1783b33e9ecc89
scoutboy420 <seba00767@gmail.com>
2017-11-18 15:14:48 +0100

    Added club struct and outputting

--- DataBase/main.c
+++ DataBase/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Club
+{
+	char name[256];
+	unsigned int league;
+	unsigned long long fansNumber;
+	unsigned long long stadiumCapacity;
+	unsigned long long budget;
+	unsigned int leaguePosition;
+};
+
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    struct Club c1 = { "abc", 2, 435435, 123123, 3453454, 1};
+
+    ShowClub(c1);
+}
+
+void ShowClub(struct Club c)
+{
+	printf("\nNazwa: %s\nLiga: %u\nLiczba kibicow: %llu\nPojemnosc stadionu: %llu\nBudzet: %llu\nPozycja w lidze: %u\n", c.name, c.league, c.fansNumber, c.stadiumCapacity, c.budget, c.leaguePosition);
 }
