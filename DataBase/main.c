c76c381c28af0de35bd2319a63037de6b10ba368
scoutboy420 <seba00767@gmail.com>
2017-11-19 14:59:19 +0100

    Added comments and small fixes

--- DataBase/main.c
+++ DataBase/main.c
@@ -4,6 +4,7 @@
 
 struct Club
 {
+	int index;
 	char name[256];
 	int league;
 	unsigned long fansNumber;
@@ -15,7 +16,7 @@ struct Club
 int Initialize(struct Club **, int *);
 struct Club * Allocate(struct Club *, int n);
 struct Club * Reallocate(struct Club *, int n);
-void ShowClub(struct Club *, int);
+void ShowClub(struct Club *);
 void ShowAll(struct Club *, int);
 int AddClub(struct Club **, int *);
 int RemoveClub(struct Club **, int *, int);
@@ -53,11 +54,12 @@ int main()
     return 0;
 }
 
+/* Dodanie przykladowych klubow na poczatku programu */
 int Initialize(struct Club **clubArrayPointer, int *length)
 {
-	struct Club c1 = { "Legia Warszawa", 1, 100000, 60000, 200000000, 1};
-	struct Club c2 = { "Wislaw Krakow" , 1, 80000, 50000, 100000000, 2};
-	struct Club c3 = { "Arka Gdynia", 1, 50000, 45000, 50000000, 3};
+	struct Club c1 = { 1, "Legia Warszawa", 1, 100000, 60000, 200000000, 1};
+	struct Club c2 = { 2, "Wislaw Krakow" , 1, 80000, 50000, 100000000, 2};
+	struct Club c3 = { 3, "Arka Gdynia", 1, 50000, 45000, 50000000, 3};
 
 	*length = 3;
 
@@ -75,50 +77,66 @@ int Initialize(struct Club **clubArrayPointer, int *length)
 	return 1;
 }
 
+/* Alokacja pamieci na tablice klubow */
 struct Club * Allocate(struct Club *clubArray, int length)
 {
 	return malloc(length * sizeof(*clubArray));
 }
 
+/* Realokacja pamieci na tablice klubow o nowej dlugosci */
 struct Club * Reallocate(struct Club *clubArray, int length)
 {
 	return realloc(clubArray, length * sizeof(*clubArray));
 }
 
-void ShowClub(struct Club *club, int index)
+/* Wypisanie informacji o klubie */
+void ShowClub(struct Club *club)
 {
-	printf("\n%d.\nNazwa: %s\nLiga: %u\nLiczba kibicow: %lu\nPojemnosc stadionu: %lu\nBudzet: %lu\nPozycja w lidze: %u\n", index+1, (*club).name, (*club).league, (*club).fansNumber, (*club).stadiumCapacity, (*club).budget, (*club).leaguePosition);
+	printf("\n%d.\nNazwa: %s\nLiga: %u\nLiczba kibicow: %lu\nPojemnosc stadionu: %lu\nBudzet: %lu\nPozycja w lidze: %u\n", (*club).index, (*club).name, (*club).league, (*club).fansNumber, (*club).stadiumCapacity, (*club).budget, (*club).leaguePosition);
 }
 
+/* Wypisanie wszystkich klubow */
 void ShowAll(struct Club *clubArray, int length)
 {
 	int i;
 
 	for(i = 0; i < length; i++)
 	{
-		ShowClub(&(clubArray[i]), i);
+		ShowClub(&(clubArray[i]));
 	}
 }
 
+/* Dodawanie nowego klubu */
 int AddClub(struct Club **clubArrayPointer, int *length)
 {
 	int n, r;
 	char c;
 	char s[256];
+	struct Club *p;
 
 	(*length)++;
 
-	*clubArrayPointer = Reallocate(*clubArrayPointer, *length);
+	p = Reallocate(*clubArrayPointer, *length);
 
-	if(*clubArrayPointer == NULL)
+	if(p == NULL)
 	{
+		(*length)--;
 		return 0;
 	}
+
+	*clubArrayPointer = p;
+
+	(*clubArrayPointer)[*length - 1].index = *length;
+
+	/* Ponizej odbieranie od uzytkownika danych
+		Jezeli uzytkownik podal bledne dane, to powtarzamy pobieranie az poda poprawne
+	*/
 
 	do
 	{
 		r = 0;
 		printf("\nPodaj nazwe klubu: ");
+		/* W nazwie klubu moga wystepowac wszystkie znaki oprocz nowej linii */
 		n = scanf(" %255[^\n]s", &((*clubArrayPointer)[*length - 1].name));
 		while(getchar() != '\n');
 
@@ -206,23 +224,43 @@ int AddClub(struct Club **clubArrayPointer, int *length)
 	return 1;
 }
 
+/* Usuwanie istniejacego klubu o podanym indeksie
+   Indeks klubu w strukturze pokrywa sie z indeksem klubu w tablicy + 1 */
 int RemoveClub(struct Club **clubArrayPointer, int *length, int index)
 {
 	int i;
+	/* Kopia zapasowa usuwanego klubu, w razie gdyby trzeba bedzie sie cofnac */
+	struct Club t = (*clubArrayPointer)[index];
+	struct Club *p;
 
 	(*length)--;
 
+
+	/* Przesuwamy wszystko wieksze od indeksu w dol */
 	for(i = index; i < *length; i++)
 	{
 		(*clubArrayPointer)[i] = (*clubArrayPointer)[i+1];
+		(*clubArrayPointer)[i].index--;
 	}
 
-	*clubArrayPointer = Reallocate(*clubArrayPointer, *length);
+	p = Reallocate(*clubArrayPointer, *length);
 
-	if(*clubArrayPointer == NULL)
+	if(p == NULL)
 	{
+		(*length)++;
+
+		for(i = *length - 1; i < index; i--)
+		{
+			(*clubArrayPointer)[i] = (*clubArrayPointer)[i-1];
+			(*clubArrayPointer)[i].index++;
+		}
+
+		(*clubArrayPointer)[index] = t;
+
 		return 0;
 	}
+
+	*clubArrayPointer = p;
 
 	return 1;
 }
