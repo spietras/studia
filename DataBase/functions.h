6c5c1a4f24a58b9f12997fabb3d2f4c1bdc70aad
scoutboy420 <seba00767@gmail.com>
2017-11-20 18:25:42 +0100

    Added file separation

---
+++ DataBase/functions.h
@@ -0,0 +1,35 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+struct Club
+{
+	int index;
+	char name[256];
+	int league;
+	unsigned long fansNumber;
+	unsigned long stadiumCapacity;
+	unsigned long budget;
+	unsigned long leaguePosition;
+};
+
+int Initialize(struct Club **, int *, struct Club **, int *);
+struct Club * Allocate(struct Club *, int n);
+struct Club * Reallocate(struct Club *, int n);
+void ShowClub(struct Club *);
+void ShowAll(struct Club *, int);
+int AddClub(struct Club **, int *);
+int RemoveClub(struct Club **, int *);
+struct Club * Search(struct Club *, int, struct Club **, int *);
+struct Club * SearchByExact(struct Club *, int, struct Club **, int *, int);
+struct Club * SearchByRange(struct Club *, int, struct Club **, int *, int);
+struct Club * SearchByMore(struct Club *, int, struct Club **, int *, int);
+struct Club * SearchByLess(struct Club *, int, struct Club **, int *, int);
+unsigned long GetValue(struct Club *, int, int);
+int ShowMenu(struct Club **, int *, struct Club **, int *);
+unsigned long ToNumber(char *, int);
+unsigned long GetNumberInput(char*, char*);
+void GetStringInput(char *, char *, char *);
+void WaitToContinue();
+unsigned long TenPower(int);
+
+#endif // FUNCTIONS_H_INCLUDED
