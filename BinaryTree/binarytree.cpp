d261b2763e579fa11049c19fb0e1eb5884dcf32a
scoutboy420 <seba00767@gmail.com>
2018-03-14 23:19:04 +0100

    Nodes number and height. Better printing. More constructors. Better comments. Minor fixes

--- BinaryTree/binarytree.cpp
+++ BinaryTree/binarytree.cpp
@@ -1,32 +1,40 @@
 #include <iostream>
+#include <algorithm>
 #include "binarytree.h"
 
 using namespace std;
 
-BinaryTree::BinaryTree(const int num)
+BinaryTree::BinaryTree(const int num) : nodeCount_(1)
 {
 	root_ = new Node(num);
 }
 
-BinaryTree::BinaryTree(const int root, const vector<int>& values)
+BinaryTree::BinaryTree(const int root, const vector<int>& values) : nodeCount_(1)
 {
 	root_ = new Node(root);
-	for(int value : values)
-		addNode(value);
+	addNode(values);
 }
 
-BinaryTree::BinaryTree(const BinaryTree& bt)
+BinaryTree::BinaryTree(const Node& root, const std::vector<Node>& nodes) : nodeCount_(1)
 {
+	root_ = new Node(root);
+	addNode(nodes);
+}
+
+BinaryTree::BinaryTree(const BinaryTree& bt) : nodeCount_(1)
+{
+	//Copies root from given binary tree
 	root_ = new Node(*bt.root_);
 }
 
-BinaryTree::BinaryTree(BinaryTree&& bt) noexcept
+BinaryTree::BinaryTree(BinaryTree&& bt) noexcept : nodeCount_(1)
 {
+	//Moves pointer to the root to this object
 	root_ = bt.root_;
 	bt.root_ = nullptr;
 }
 
-BinaryTree::BinaryTree(const Node& node)
+BinaryTree::BinaryTree(const Node& node) : nodeCount_(1)
 {
 	root_ = new Node(node);
 }
@@ -38,9 +46,11 @@ BinaryTree::~BinaryTree()
 
 BinaryTree& BinaryTree::operator=(const BinaryTree& bt)
 {
+	//Check if object is not assigning to itself
 	if(this == &bt)
 		return *this;
 
+	//Delete current root and make new
 	delete root_;
 	root_ = new Node(*bt.root_);
 	return *this;
@@ -48,17 +58,19 @@ BinaryTree& BinaryTree::operator=(const BinaryTree& bt)
 
 BinaryTree& BinaryTree::operator=(BinaryTree&& bt) noexcept
 {
+	//Delete current root and just "steal" existing root from given object 
 	delete root_;
 	root_ = bt.root_;
 	bt.root_ = nullptr;
 	return *this;
 }
 
-void BinaryTree::addNode(const int num) const
+void BinaryTree::addNode(const int num)
 {
 	if(root_->num_ == num)
 		return;
 
+	//Find parent node
 	Node* p = getNode(root_, num);
 
 	if(p->num_ == num)
@@ -67,19 +79,27 @@ void BinaryTree::addNode(const int num) const
 	if(p->num_ > num)
 	{
 		p->setLeftChild(num);
+		nodeCount_++;
 		return;
 	}
 
 	p->setRightChild(num);
+	nodeCount_++;
 }
 
-void BinaryTree::addNode(const std::vector<int>& values) const
+void BinaryTree::addNode(const std::vector<int>& values)
 {
 	for(int value : values)
 		addNode(value);
 }
 
-void BinaryTree::addNode(Node& node) const
+void BinaryTree::addNode(const std::vector<Node>& nodes)
+{
+	for(Node n : nodes)
+		addNode(n);
+}
+
+void BinaryTree::addNode(Node& node)
 {
 	auto nodes = node.getNodesCopies();
 
@@ -89,7 +109,7 @@ void BinaryTree::addNode(Node& node) const
 
 Node BinaryTree::findNodeCopy(const int num) const
 {
-	return Node(*getNode(root_, num));
+	return Node(*getNode(root_, num)); //Always returns some node
 }
 
 Node* BinaryTree::getNode(Node* currentNode, const int num)
@@ -111,11 +131,46 @@ Node* BinaryTree::getNode(Node* currentNode, const int num)
 	return currentNode;
 }
 
+int BinaryTree::getHeight(Node* n) const
+{
+	if (!n->rightChild_ && !n->leftChild_)
+		return 1;
+
+	if (n->leftChild_ && n->rightChild_)
+		return 1+max(getHeight(n->leftChild_), getHeight(n->rightChild_));
+
+	if (n->leftChild_)
+		return 1+getHeight(n->leftChild_);
+
+	return 1+getHeight(n->rightChild_);
+}
+
 void BinaryTree::printPretty() const
 {
 	cout << endl << "Binary Tree Structure: " << endl;
 
 	printNode(root_, 0);
+
+	cout << endl;
+}
+
+void BinaryTree::printAscending() const
+{
+	cout << endl << "Nodes in ascending order: " << endl;
+	auto nodes = root_->getNodesCopies();
+	for(Node n : nodes)
+		cout << " " << n.num_ << " ";
+
+	cout << endl;
+}
+
+void BinaryTree::printDescending() const
+{
+	cout << endl << "Nodes in descending order: " << endl;
+	auto nodes = root_->getNodesCopies();
+	reverse(nodes.begin(), nodes.end());
+	for(Node n : nodes)
+		cout << " " << n.num_ << " ";
 
 	cout << endl;
 }
--- BinaryTree/binarytree.h
+++ BinaryTree/binarytree.h
@@ -6,30 +6,46 @@ class BinaryTree
 {
 private:
 	Node* root_;
+	int nodeCount_;
 
 	static void printNode(const Node*, int);
 	static void indent(int);
 	static Node* getNode(Node*, int);
+	int getHeight(Node*) const;
 public:
 	//Constructors
+
 	explicit BinaryTree(int);
 	BinaryTree(int, const std::vector<int>&);
-	BinaryTree(const BinaryTree&);
-	BinaryTree(BinaryTree&&) noexcept;
+	BinaryTree(const Node&, const std::vector<Node>&);
+	BinaryTree(const BinaryTree&); //Copy constructor
+	BinaryTree(BinaryTree&&) noexcept; //Move constructor
 	explicit BinaryTree(const Node&);
 
 	//Destructor
 	~BinaryTree();
 
 	//Operators
-	BinaryTree& operator=(const BinaryTree&);
-	BinaryTree& operator=(BinaryTree&&) noexcept;
+
+	BinaryTree& operator=(const BinaryTree&); //Copy assignment
+	BinaryTree& operator=(BinaryTree&&) noexcept; //Move assignment
 
 	Node getRootCopy() const { return Node(*root_); }
+	int getHeight() const { return getHeight(root_); }
+	int getNodeCount() const { return nodeCount_; }
 
-	void addNode(int) const;
-	void addNode(const std::vector<int>&) const;
-	void addNode(Node&) const;
+	//Adding
+
+	void addNode(int);
+	void addNode(const std::vector<int>&);
+	void addNode(const std::vector<Node>&);
+	void addNode(Node&);
+
 	Node findNodeCopy(int) const;
+
+	//Output
+
 	void printPretty() const;
+	void printAscending() const;
+	void printDescending() const;
 };
