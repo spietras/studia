744aade18b492123e984ffeae0f34dfa17665b3c
scoutboy420 <seba00767@gmail.com>
2018-03-14 20:47:25 +0100

    Fixes

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -6,9 +6,21 @@ using namespace std;
 
 int main()
 {
-	BinaryTree* b = new BinaryTree(5, {2, 8, 4, 7, 3, 1, 6, 9});
+	BinaryTree b(5, {2, 8, 4, 7, 3, 1, 6, 9});
+	BinaryTree a = b;
+	BinaryTree c(3);
+	c = b;
 
-	b->print();
+	b.addNode({100, 101, 102, -100, -101, -102});
+
+	a.addNode({69, 68, 70, -69, -68, -70});
+	c.addNode({420, 421, 422, -420, -421, -422});
+
+	auto t = b.getRootCopy().getNodesCopies();
+
+	b.printPretty();
+	a.printPretty();
+	c.printPretty();
+
 	cin.ignore();
-	delete b;
 }
