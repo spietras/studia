257fc4cd6d37d4b028e00d9aac97f1ee7dfed6ad
Sebastian Pietras <seba00767@gmail.com>
2019-05-23 13:06:43 +0200

    Scrolling in picture details

--- src/main/java/com/spietras/picgallery/picdetails/PicDetailsController.java
+++ src/main/java/com/spietras/picgallery/picdetails/PicDetailsController.java
@@ -3,13 +3,16 @@ package com.spietras.picgallery.picdetails;
 import com.spietras.picgallery.picdetails.models.PicDetailsModel;
 import com.spietras.picgallery.search.SearchController;
 import com.spietras.picgallery.utils.ExecutorManager;
+import com.spietras.picgallery.utils.ZoomHelper;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.StackPane;
 
 import java.util.concurrent.ExecutorService;
@@ -29,6 +32,8 @@ public class PicDetailsController
     @FXML
     private ImageView fullImageView;
 
+    private double originalWidth;
+
     public PicDetailsController(SearchController searchController, PicDetailsModel model)
     {
         this.searchController = searchController;
@@ -44,6 +49,9 @@ public class PicDetailsController
                 fullImageView.setFitHeight(fullImage.getHeight());
                 fullImageView.setFitWidth(fullImage.getWidth());
                 fullImageView.setImage(fullImage);
+
+                originalWidth = fullImage.getWidth();
+                stackPane.setOnScroll(this::onScroll);
             });
         });
     }
@@ -63,6 +71,22 @@ public class PicDetailsController
         catch(IllegalStateException e)
         {
             e.printStackTrace();
+        }
+    }
+
+    private void onScroll(ScrollEvent e)
+    {
+        if(e.isControlDown()) //zoom with ctrl pressed
+        {
+            final double MIN_ZOOM = 0.2, MAX_ZOOM = 5.0;
+            final double scaleFactor = 1.2;
+
+            double zoomFactor = e.getDeltaY() > 0 ? scaleFactor : 1 / scaleFactor;
+
+            ZoomHelper.zoomImageViewInsideScrollPane(fullImageView, new Point2D(e.getX(), e.getY()), scrollPane,
+                                                     zoomFactor, MIN_ZOOM, MAX_ZOOM, originalWidth);
+
+            e.consume(); //consume event so that scrollpane can't treat it as scroll
         }
     }
 }
