29bf196a227cebade4dac426d17cf71816d8c106
Sebastian Pietras <seba00767@gmail.com>
2019-05-29 00:43:08 +0200

    Counting symbol frequencies

--- inż/4/ARKO/x86/src/huffmanEncode.asm
+++ inż/4/ARKO/x86/src/huffmanEncode.asm
@@ -1,11 +1,45 @@
 global huffmanEncode
 
+SYMBOLS                     equ     256
+SIZE_OF_FILE_LENGTH         equ     4
+SIZE_OF_SYMBOL              equ     1
+SIZE_OF_SYMBOL_FREQUENCY    equ     4
+
 section .bss
-	var:			resb 1
+	input:			        resq    1       ; char*
+    inputSize:              resq    1       ; long
+    output:                 resq    1       ; char*
+    maxOutputSize:          resq    1       ; long
+
+    frequencies:            resq    SYMBOLS
 
 section .text
 
 huffmanEncode:
-    mov BYTE[var], 1
-    movzx EAX, BYTE[var]
+    push rbp
+    mov rbp, rsp
+
+    push r8
+    push r9
+
+    mov [input], rdi
+    mov [inputSize], rsi
+    mov [output], rdx
+    mov [maxOutputSize], rcx
+
+    mov rcx, [inputSize]                    ; counter for loop
+    mov r8, [input]                         ; start of input
+countFrequenciesLoop:
+    movzx r9, byte[r8]                      ; load character from input
+    inc qword[r9 + frequencies]             ; increase its frequency
+    inc r8                                  ; go to the next character
+    loop countFrequenciesLoop
+
+
+
+    movzx rax, byte[frequencies + 10]
+
+    pop r9
+    pop r8
+    pop rbp 
     ret
