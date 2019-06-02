955840736147af803e6e9d21f047eb7126190b2f
Sebastian Pietras <seba00767@gmail.com>
2019-06-02 15:31:38 +0200

    Building Huffman tree

--- inż/4/ARKO/x86/src/huffmanDecode.asm
+++ inż/4/ARKO/x86/src/huffmanDecode.asm
@@ -32,8 +32,10 @@ section .bss
     originalSize:           resq    1
 
     treenodes:              resb    TREENODES_SIZE
-    treenodesLeaves:        resb    1
+    treenodesLeaves:        resw    1
     treenodesNodes:         resw    1
+
+    currentInputPos:        resq    1
 
 section .text
 
@@ -59,11 +61,11 @@ huffmanDecode:
     mov qword[originalSize], r8                                                             ; load original file size
 
     add rdi, QWORD_SIZE
-    mov r8b, byte[rdi]
-    mov byte[treenodesLeaves], r8b                                                          ; load how many symbols were used
-    movzx rcx, r8b
+    mov r8w, word[rdi]
+    mov word[treenodesLeaves], r8w                                                          ; load how many symbols were used
+    movzx rcx, r8w
 
-    add rdi, SYMBOL_SIZE
+    add rdi, WORD_SIZE
     mov r10, treenodes
 pushLeavesLoop:                                 
     mov r8b, byte[rdi]                                                                      ; load symbol
@@ -75,6 +77,114 @@ pushLeavesLoop:
     add r10, TREENODE_SIZE
     add rdi, SYMBOL_SIZE + FREQUENCY_SIZE
     loop pushLeavesLoop
+
+    mov qword[currentInputPos], rdi
+
+    mov r8w, word[treenodesLeaves]
+    mov word[treenodesNodes], r8w
+
+    mov r13, r10                                                                            ; end of treenodes - add new nodes here
+buildTreeLoop:
+    mov rcx, treenodes
+    mov r8, 0                                                                               ; first node found
+    mov r9, 0                                                                               ; second node found
+    mov r12w, 0                                                                             ; node counter
+    mov r11, 0xFFFFFFFF                                                                     ; smallest found ("infinity" at first)
+findTwoNodesLoop:
+
+    cmp r12w, word[treenodesNodes]
+    jge endFindTwoNodesLoop                                                                 ; if all nodes searched, end
+
+    mov r10b, byte[rcx + SYMBOL_SIZE + FREQUENCY_SIZE + 3*QWORD_SIZE]                       ; used flag
+    cmp r10b, 1
+    je repeatTwoNodesLoop                                                                   ; if used, continue
+
+    cmp r8, 0
+    je afterFreqCheck                                                                       ; if two nodes not found yet, ignore frequency check
+
+    mov r10, qword[rcx + SYMBOL_SIZE]                                                       ; frequency
+    cmp r10, r11
+    jg repeatTwoNodesLoop                                                                   ; if frequenct greater than the smallest frequency, continue
+
+afterFreqCheck:
+    jmp nodeFound
+repeatTwoNodesLoop:
+    add rcx, TREENODE_SIZE
+    inc r12w
+    jmp findTwoNodesLoop                                                                    ; go to next node and loop
+nodeFound:
+
+    cmp r8, 0
+    je checkSecondEmpty
+    jmp bothUsed
+checkSecondEmpty:
+    cmp r9, 0
+    je bothEmpty
+    jmp oneEmpty
+
+bothEmpty:
+    mov r9, rcx                                                                             ; if no nodes foud before, set found to r9
+    jmp afterSort
+oneEmpty:
+    mov r14, qword[r9 + SYMBOL_SIZE]
+    cmp r10, r14
+    jl shiftWhenOneEmpty
+    mov r8, rcx                                                                             ; if one node found before but it's weight is bigger than found, set found to r8
+    jmp afterSort
+shiftWhenOneEmpty:
+    mov r8, r9                                                                              ; if weight is smaller, shift r9 to r8 and set found to r9
+    mov r9, rcx
+    jmp afterSort
+bothUsed:
+    mov r8, r9                                                                              ; if both used, they are sorted, so shift r9 to r8 and set found to r8
+    mov r9, rcx
+    jmp afterSort
+afterSort:
+
+    cmp r8, 0
+    je afterSetSmallest                                                                     ; don't set smallest until two nodes found
+setSmallest:
+    mov r11, qword[r8 + SYMBOL_SIZE]
+afterSetSmallest:
+    inc r12w
+
+    add rcx, TREENODE_SIZE
+    jmp findTwoNodesLoop                                                                    ; go to next node and loop
+endFindTwoNodesLoop:
+
+    cmp r8, 0
+    je endBuildTreeLoop                                                                     ; if end and only one node found, tree is complete
+
+    mov r14, qword[r8 + SYMBOL_SIZE]
+    mov r15, qword[r9 + SYMBOL_SIZE]
+    add r14, r15
+    mov qword[r13 + SYMBOL_SIZE], r14                                                       ; set new node frequency to sum of found nodes' frequencies
+
+    mov r14, qword[r8 + SYMBOL_SIZE]
+    cmp r14, r15
+    jg firstNodeGreater
+    jmp secondNodeGreater
+
+firstNodeGreater:
+    mov qword[r13 + SYMBOL_SIZE + FREQUENCY_SIZE], r9                                       ; set left child
+    mov qword[r13 + SYMBOL_SIZE + FREQUENCY_SIZE + QWORD_SIZE], r8                          ; set right child
+    jmp afterSetChildren
+secondNodeGreater:
+    mov qword[r13 + SYMBOL_SIZE + FREQUENCY_SIZE], r8                                       ; set left child
+    mov qword[r13 + SYMBOL_SIZE + FREQUENCY_SIZE + QWORD_SIZE], r9                          ; set right child
+    jmp afterSetChildren
+
+afterSetChildren:
+    mov qword[r8 + SYMBOL_SIZE + FREQUENCY_SIZE + QWORD_SIZE*2], r13                        ; set parent in first found
+    mov byte[r8 + SYMBOL_SIZE + FREQUENCY_SIZE + QWORD_SIZE*3], 1                           ; set used in first found
+    mov qword[r9 + SYMBOL_SIZE + FREQUENCY_SIZE + QWORD_SIZE*2], r13                        ; set parent in second found
+    mov byte[r9 + SYMBOL_SIZE + FREQUENCY_SIZE + QWORD_SIZE*3], 1                           ; set used in second found
+
+    inc word[treenodesNodes]
+    add r13, TREENODE_SIZE
+    jmp buildTreeLoop                                                                       ; increase nodes and loop
+
+endBuildTreeLoop:
 
     mov rax, 0
 
