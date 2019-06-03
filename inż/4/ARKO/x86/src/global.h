5e029c6e00b0471c03f4cb298c39f28308223378
Sebastian Pietras <seba00767@gmail.com>
2019-06-03 13:24:27 +0200

    Refactored and adjusted max header size

--- inż/4/ARKO/x86/src/global.h
+++ inż/4/ARKO/x86/src/global.h
@@ -1,8 +1,14 @@
 //sizes in bytes
 
-#define SYMBOLS 256
-#define SIZE_OF_FILE_LENGTH 4
-#define SIZE_OF_SYMBOL 1
-#define SIZE_OF_SYMBOL_FREQUENCY 4
+#define BYTE_SIZE                   1
+#define WORD_SIZE                   2
+#define DWORD_SIZE                  4
+#define QWORD_SIZE                  8
 
-#define MAXHEADERSIZE (SIZE_OF_FILE_LENGTH + (SIZE_OF_SYMBOL + SIZE_OF_SYMBOL_FREQUENCY)*SYMBOLS)
+#define SYMBOLS 256
+#define SIZE_OF_FILE_LENGTH         QWORD_SIZE
+#define SIZE_OF_SYMBOL              BYTE_SIZE
+#define SIZE_OF_SYMBOLS_COUNT       WORD_SIZE
+#define SIZE_OF_SYMBOL_FREQUENCY    QWORD_SIZE
+
+#define MAXHEADERSIZE               (SIZE_OF_FILE_LENGTH + SIZE_OF_SYMBOLS_COUNT + (SIZE_OF_SYMBOL + SIZE_OF_SYMBOL_FREQUENCY)*SYMBOLS)
