e99b57aaaf5b781374d80132c74ee74aa585b6c6
Sebastian Pietras <seba00767@gmail.com>
2019-11-13 19:28:33 +0100

    Added graph representation of data

---
+++ graph.py
@@ -0,0 +1,15 @@
+class Graph:
+    def __init__(self, v, edges):
+        self._v = v
+
+        self._adj_list = {i:set() for i in range(1,v+1)}
+
+        for begin, end in edges:
+            self._adj_list[begin].add(end)
+            self._adj_list[end].add(begin)
+
+    def get_neighbours(self, node):
+        return self._adj_list[node]
+
+    def get_nodes(self):
+        return set(self._adj_list.keys())
