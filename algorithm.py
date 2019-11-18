9302c9876f9bb8e6a1b4e9718c0c9a8bbc10bb7a
Sebastian Pietras <seba00767@gmail.com>
2019-11-18 01:41:17 +0100

    Returning edge instead of boolean

--- algorithm.py
+++ algorithm.py
@@ -12,15 +12,17 @@ def get_graph_parts(graph):
     set2 = set()
     
     while nodes:
-        bipartite, subset1, subset2 = get_subtree_parts(graph, nodes.pop())
-        if not bipartite:
-            return False, set1, set2
+        bad_edge, subset1, subset2 = get_subtree_parts(graph, nodes.pop())
         
         set1.update(subset1)
         set2.update(subset2)
+        
+        if bad_edge:
+            return bad_edge, set1, set2
+        
         nodes.difference_update(subset1, subset2)
         
-    return True, set1, set2
+    return None, set1, set2
 
 def get_subtree_parts(graph, start_node):
     color_a = {start_node}
@@ -35,10 +37,10 @@ def get_subtree_parts(graph, start_node):
     
         for neighbour in graph.get_neighbours(node):
             if neighbour in this_color:
-                return False, color_a, color_b
+                return (node, neighbour), color_a, color_b
             
             if neighbour not in other_color:
                 other_color.add(neighbour)
                 queue.append(neighbour)
                 
-    return True, color_a, color_b
+    return None, color_a, color_b
