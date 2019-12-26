b3e562b0f27fd273e0ee247ff745822ca52f9f48
Sebastian Pietras <01133337@pw.edu.pl>
2019-12-26 12:20:43 +0100

    Added simple code with empty window

---
+++ inż/5/GKOM/opengl-windmill/src/main.cpp
@@ -0,0 +1,39 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+GLFWwindow* initWindow(unsigned int width, unsigned int height, const char* title)
+{
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+	glewInit();
+
+	glViewport(0, 0, width, height);
+
+	return window;
+}
+
+int main()
+{
+    GLFWwindow* window = initWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
