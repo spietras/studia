65533737385505e6f49c111e25c0534c04fcd1ef
Sebastian Pietras <seba00767@gmail.com>
2020-01-13 04:04:58 +0100

    Changed measured time to miliseconds

--- measuring.py
+++ measuring.py
@@ -1,14 +1,14 @@
 import timeit
-import statistics
 
 
 def measure(algorithm_fun, n_repeats=1):
-    return timeit.timeit(algorithm_fun, number=n_repeats) / n_repeats
+    return 1000.0 * timeit.timeit(algorithm_fun, number=n_repeats) / n_repeats
 
 
 def measure_generated(algorithm_fun, generator_fun, outer_repeats=1, inner_repeats=1):
     times_sum = 0.0
     for _ in range(outer_repeats):
         def measured_fun(data=generator_fun()): algorithm_fun(data)
+
         times_sum += measure(measured_fun, inner_repeats)
     return times_sum / outer_repeats
