9c6d433393b48306481dd000634b3db22400947f
Sebastian Pietras <seba00767@gmail.com>
2020-01-13 04:06:21 +0100

    Added comments with complexity

--- graph.py
+++ graph.py
@@ -1,15 +1,15 @@
 class Graph:
-    def __init__(self, v, edges):
+    def __init__(self, v, edges):  # O(v + e) average
         self._v = v
 
-        self._adj_list = {i: set() for i in range(1, v + 1)}
+        self._adj_list = {i: set() for i in range(1, v + 1)}  # loop v times: O(v)
 
-        for begin, end in edges:
-            self._adj_list[begin].add(end)
-            self._adj_list[end].add(begin)
+        for begin, end in edges:  # loop e times: O(e) average
+            self._adj_list[begin].add(end)  # dict access and set add: O(1) average
+            self._adj_list[end].add(begin)  # same as above
 
     def get_neighbours(self, node):
-        return self._adj_list[node]
+        return self._adj_list[node]  # O(1) average
 
     def get_nodes(self):
-        return set(self._adj_list.keys())
+        return set(self._adj_list.keys())  # list to set: O(v)
