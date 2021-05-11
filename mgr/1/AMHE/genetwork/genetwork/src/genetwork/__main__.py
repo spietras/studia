5b900c151136a5a1b4ae58fa6496e34a6ea75ea8
Sebastian Pietras <01133337@pw.edu.pl>
2021-05-11 22:38:42 +0200

    Added network parsing

--- mgr/1/AMHE/genetwork/genetwork/src/genetwork/__main__.py
+++ mgr/1/AMHE/genetwork/genetwork/src/genetwork/__main__.py
@@ -5,19 +5,25 @@ This module provides basic CLI entrypoint.
 """
 
 import sys
+from numbers import Real
+from pathlib import Path
 from typing import Optional
 
 import typer
-from genetwork.subpackage.module import get_something
+from genetwork.networks import SndlibNetworkXMLParser
 
 cli = typer.Typer()  # this is actually callable and thus can be an entry point
 
 
 @cli.command()
-def main(x: int = typer.Option(default=1, help="Dummy argument.")) -> Optional[int]:
+def main(network_path: Path = typer.Argument(...,
+                                             readable=True,
+                                             help="Path to graph definition file")) -> Optional[int]:
     """Command line interface for genetwork."""
 
-    typer.echo(get_something(x))  # typer.echo instead of print, because it's better
+    with open(network_path) as n:
+        network = SndlibNetworkXMLParser[str, Real]().parse(n)
+    typer.echo(str(network))
 
 
 if __name__ == '__main__':
--- mgr/1/AMHE/genetwork/genetwork/tests/functional/test_genetwork.py
+++ mgr/1/AMHE/genetwork/genetwork/tests/functional/test_genetwork.py
@@ -5,6 +5,7 @@ That's because functional tests should test the code from "outside".
 So we don't need to follow any hierarchy.
 
 """
+from pathlib import Path
 
 import pytest
 from genetwork.__main__ import cli
@@ -13,25 +14,24 @@ from typer.testing import CliRunner
 
 class TestGenetwork:
 
-    # pytest fixture, passed to all methods by argument
     @pytest.fixture(autouse=True, scope="class")
-    def runner(self):
+    def runner(self) -> CliRunner:
         return CliRunner()
 
-    def test_genetwork_returns_zero(self, runner):
-        result = runner.invoke(cli)
+    def test_genetwork_returns_zero_for_polska(self, runner: CliRunner, resources_dir: Path):
+        result = runner.invoke(cli, [str(resources_dir / 'polska.xml')])
         assert not result.exception
         assert result.exit_code == 0
 
-    def test_genetwork_prints_something(self, runner):
+    def test_genetwork_returns_zero_for_germany50(self, runner: CliRunner, resources_dir: Path):
+        result = runner.invoke(cli, [str(resources_dir / 'germany50.xml')])
+        assert not result.exception
+        assert result.exit_code == 0
+
+    def test_genetwork_prints_something(self, runner: CliRunner):
         result = runner.invoke(cli)
         assert result.output
 
-    def test_genetwork_prints_help(self, runner):
+    def test_genetwork_prints_help(self, runner: CliRunner):
         result = runner.invoke(cli, ['--help'])
         assert "Usage" in result.output
-
-    def test_genetwork_works_with_arg(self, runner):
-        result = runner.invoke(cli, ['--x', 2])
-        assert not result.exception
-        assert result.exit_code == 0
