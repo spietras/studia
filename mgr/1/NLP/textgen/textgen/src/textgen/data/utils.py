7052c7430a90b07c49065f565ed93256f2e4b35a
Sebastian Pietras <01133337@pw.edu.pl>
2021-05-24 23:46:52 +0200

    Data refactor

--- mgr/1/NLP/textgen/textgen/src/textgen/data/utils.py
+++ mgr/1/NLP/textgen/textgen/src/textgen/data/utils.py
@@ -1,28 +1,11 @@
 from abc import ABC, abstractmethod
 from io import StringIO
 from pathlib import Path
-from typing import Iterator, TextIO, Dict, List
+from typing import Iterator, TextIO, Dict, Any, Callable, List
 
 import nltk
-from bidict import bidict
 from nltk.tokenize.treebank import TreebankWordDetokenizer, TreebankWordTokenizer
 from ordered_set import OrderedSet
-
-
-class Counter:
-    """Counter utility for mutable counting."""
-
-    def __init__(self, count: int = 0) -> None:
-        super().__init__()
-        self.count = count
-
-    def inc(self) -> 'Counter':
-        self.count += 1
-        return self
-
-    def dec(self) -> 'Counter':
-        self.count -= 1
-        return self
 
 
 class Tokenizer(ABC):
@@ -114,41 +97,36 @@ class FilesInDirectoryStreamGenerator(FilesStreamGenerator):
 class Corpus:
     """Corpus base class."""
 
-    _tokens: OrderedSet[str] = OrderedSet()
+    _vocabulary: OrderedSet[str] = OrderedSet()
 
-    def __init__(self, tokens: OrderedSet[str]) -> None:
+    def __init__(self, vocabulary: OrderedSet[str]) -> None:
         super().__init__()
-        self._tokens = tokens
+        self._vocabulary = vocabulary
 
     def add_extra_token(self, token: str) -> None:
-        self._tokens.add(token)
+        self._vocabulary.add(token)
 
     def add_extra_tokens(self, tokens: Iterator[str]) -> None:
         for token in tokens:
             self.add_extra_token(token)
 
     def change_tokens(self, new: OrderedSet[str]) -> None:
-        self._tokens = new
+        self._vocabulary = new
 
     @property
-    def tokens(self) -> OrderedSet[str]:
+    def vocabulary(self) -> OrderedSet[str]:
         """All tokens available in a corpus. """
-        return self._tokens
-
-    @property
-    def vocabulary(self) -> bidict[int, str]:
-        """Bidirectional mapping from indices to tokens (and vice versa)."""
-        return bidict({i: word for i, word in enumerate(self.tokens)})
+        return self._vocabulary
 
     @property
     def indices_to_tokens(self) -> Dict[int, str]:
         """Mapping from indices to tokens."""
-        return dict(self.vocabulary)
+        return {i: word for i, word in enumerate(self.vocabulary)}
 
     @property
     def tokens_to_indices(self) -> Dict[str, int]:
         """Mapping from tokens to indices."""
-        return dict(self.vocabulary.inverse)
+        return {word: i for i, word in enumerate(self.vocabulary)}
 
     def convert_to_token(self, indices: Iterator[int]) -> Iterator[str]:
         """Converts indices to tokens."""
@@ -159,3 +137,76 @@ class Corpus:
         """Converts tokens to indices."""
         tti = self.tokens_to_indices
         return (tti[token] for token in tokens)
+
+
+class CorpusEvaluator:
+    """Create Corpus from streams with additional hooks."""
+
+    def __init__(self, sentence_tokenizer: Tokenizer, word_tokenizer: Tokenizer) -> None:
+        super().__init__()
+        self.sentence_tokenizer = sentence_tokenizer
+        self.word_tokenizer = word_tokenizer
+        self.sentence_hooks = []
+        self.word_hooks = []
+
+    def on_sentence(self, hook: Callable[[str], Any]) -> None:
+        self.sentence_hooks.append(hook)
+
+    def on_word(self, hook: Callable[[str], Any]) -> None:
+        self.word_hooks.append(hook)
+
+    def evaluate(self, streams: Iterator[TextIO]) -> Corpus:
+        vocabulary = OrderedSet()
+        for stream in streams:
+            for sentence in self.sentence_tokenizer.tokenize(stream.read()):
+                for hook in self.sentence_hooks:
+                    hook(sentence)
+                for word in self.word_tokenizer.tokenize(sentence):
+                    vocabulary.append(word)
+                    for hook in self.word_hooks:
+                        hook(word)
+        return Corpus(vocabulary)
+
+
+class CompletionCorpus(Corpus):
+    """Corpus with added pad, start and end tokens."""
+
+    def __init__(self, vocabulary: OrderedSet[str],
+                 pad_token: str = "<P>", start_token: str = "<S>", end_token: str = "<E>") -> None:
+        super().__init__(OrderedSet([pad_token, start_token, end_token]) | vocabulary)
+        self.pad_token = pad_token
+        self.start_token = start_token
+        self.end_token = end_token
+        self.pad_id = self.tokens_to_indices[pad_token]
+        self.start_id = self.tokens_to_indices[start_token]
+        self.end_id = self.tokens_to_indices[end_token]
+
+
+class SentencePadder:
+    """Utility class for padding sentences."""
+
+    def __init__(self, max_length: int, corpus: CompletionCorpus) -> None:
+        super().__init__()
+        self.max_length = max_length
+        self.corpus = corpus
+
+    def pad_with_token(self, sentence: List[str]) -> List[str]:
+        return sentence + [self.corpus.pad_token] * max(0, (self.max_length - len(sentence)))
+
+    def pad_with_index(self, sentence: List[int]) -> List[int]:
+        return sentence + [self.corpus.pad_id] * max(0, (self.max_length - len(sentence)))
+
+
+class SentenceCompletionConfig:
+    """Configuration holder for sentence completion."""
+
+    def __init__(self, corpus: CompletionCorpus,
+                 sentence_tokenizer: Tokenizer,
+                 word_tokenizer: Tokenizer,
+                 max_length: int) -> None:
+        super().__init__()
+        self.corpus = corpus
+        self.sentence_tokenizer = sentence_tokenizer
+        self.word_tokenizer = word_tokenizer
+        self.max_length = max_length
+        self.padder = SentencePadder(max_length, corpus)
