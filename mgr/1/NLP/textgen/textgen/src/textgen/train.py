0e2d98b7abd1de828858c43894518bb2a5e60639
Sebastian Pietras <01133337@pw.edu.pl>
2021-05-28 23:09:25 +0200

    Added seeding

--- mgr/1/NLP/textgen/textgen/src/textgen/train.py
+++ mgr/1/NLP/textgen/textgen/src/textgen/train.py
@@ -10,7 +10,7 @@ from typing import Optional
 
 import torch
 import typer
-from pytorch_lightning import Trainer
+from pytorch_lightning import Trainer, seed_everything
 from textgen.data.modules import SentenceCompletionIterableSplitFromDir
 from textgen.model.pickers import IndexPickerType, get_picker
 from textgen.model.transformer import Transformer, TransformerModel
@@ -29,18 +29,22 @@ def train(dataset_path: Path = typer.Argument(..., file_okay=False, readable=Tru
           num_gpus: int = typer.Option(int(torch.cuda.is_available()), "--num-gpus", "-g", help="Number of GPUs."),
           max_epochs: int = typer.Option(30, "--max-epochs", "-e", help="Maximum number of training epochs."),
           lr: float = typer.Option(0.01, help="Learning rate."),
-          d_model: int = typer.Option(64, "--d-model", "-dm", help="The size of the vector representing one token."),
-          d_ff: int = typer.Option(16, "--d-ff", "-df", help="The size of the first layer in the FFN in each layer "
-                                                             "of the encoder/decoder stack."),
+          d_model: int = typer.Option(64, "--d-model", "-m", help="The size of the vector representing one token."),
+          d_ff: int = typer.Option(16, "--d-ff", "-f", help="The size of the first layer in the FFN in each layer "
+                                                            "of the encoder/decoder stack."),
           num_heads: int = typer.Option(4, "--num-heads", "-h", help="The number of attention heads."),
           num_layers: int = typer.Option(4, "--num-layers", "-l", help="The number of encoder/decoder stack layers."),
           drop_out_rate: float = typer.Option(0.1, "--dropout-rate", "-d", help="Dropout rate."),
           index_picker: IndexPickerType = typer.Option(IndexPickerType.greedy, "--index-picker", "-p",
                                                        help="Type of index picking"),
           teacher_forcing_val: bool = typer.Option(False, "--teacher-forcing-val", "-t",
-                                                   help="Use teacher forcing in validation and test stages")
+                                                   help="Use teacher forcing in validation and test stages"),
+          seed: Optional[int] = typer.Option(None, "--seed", "-r", help="Seed for reproducibility")
           ) -> Optional[int]:
     """Command line interface for textgen-train."""
+
+    if seed is not None:
+        seed_everything(seed, workers=True)
 
     print("Evaluating dataset...")
     datamodule = SentenceCompletionIterableSplitFromDir(dataset_path, max_length=max_length, batch_size=batch_size,
@@ -56,7 +60,8 @@ def train(dataset_path: Path = typer.Argument(..., file_okay=False, readable=Tru
     else:
         system = Transformer.load_from_checkpoint(str(ckpt_path))
 
-    trainer = Trainer(max_epochs=max_epochs, gpus=num_gpus, resume_from_checkpoint=ckpt_path)
+    trainer = Trainer(max_epochs=max_epochs, gpus=num_gpus,
+                      resume_from_checkpoint=ckpt_path, deterministic=seed is not None)
 
     # Train
     print("Training...")
