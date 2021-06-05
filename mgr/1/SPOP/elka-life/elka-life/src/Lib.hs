1380eebb6a700a970d09902dca7dc752178db924
Wojciech Krupski <48606886+wkru@users.noreply.github.com>
2021-06-05 20:14:59 +0200

    Implement basic program functions

--- mgr/1/SPOP/elka-life/elka-life/src/Lib.hs
+++ mgr/1/SPOP/elka-life/elka-life/src/Lib.hs
@@ -1,6 +1,40 @@
 module Lib
-    ( someFunc
+    ( start
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "Hello World"
+import System.Exit
+
+data GameState = GameState
+  {questScores :: [Maybe Int]
+  } deriving Show
+
+setScore :: [Maybe Int] -> Int -> Int -> [Maybe Int]
+setScore (x:xs) score pos = case pos of 
+                                0 -> Just score : xs
+                                _ -> x : (setScore xs score (pos - 1))
+
+start :: IO ()
+start = faculty (GameState [Nothing, Nothing])
+
+questFaculty :: GameState -> IO GameState
+questFaculty state = do putStrLn "questFaculty"
+                        return (GameState (setScore (questScores state) 10 0))
+
+faculty :: GameState -> IO ()
+faculty state = do putStrLn "faculty"
+                   print state
+                   input <- getLine
+                   case input of
+                       "quest" -> do newState <- (questFaculty state)
+                                     print newState
+                                     faculty newState
+                       "dorm" -> dorm state
+                       _ -> do putStrLn "quest or dorm"
+                               faculty state
+
+
+dorm :: GameState -> IO ()
+dorm state = do putStrLn "dorm" 
+                input <- getLine
+                faculty state
+
