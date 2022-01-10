9af8773b091ab0cf6b6f3fcbde533c8d8b8760eb
Sebastian Pietras <spietras1998@gmail.com>
2022-01-10 20:24:02 +0100

    Added initial `Hoppscotch` configuration (#11)
    * Added initial Hoppscotch configuration
    
    * Added workflow for testing Hoppscotch Docker builds

---
+++ mgr/2/SPDB/retrail/api/hoppscotch/src/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+print_usage() {
+  # Prints script usage
+
+  cat <<EOF
+Usage: $0 [-h]
+    -h, --help                                prints this message
+EOF
+}
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+  -h | --help)
+    print_usage
+    exit
+    ;;
+  esac
+  shift
+done
+
+cd /app/
+pnpm run start
