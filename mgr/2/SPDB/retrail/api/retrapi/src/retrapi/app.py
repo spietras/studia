7722928a5a51d50406bdc0ce1982a204636d4875
Sebastian Pietras <spietras1998@gmail.com>
2022-01-14 08:35:37 +0100

    Added `border` endpoint (#21)

--- mgr/2/SPDB/retrail/api/retrapi/src/retrapi/app.py
+++ mgr/2/SPDB/retrail/api/retrapi/src/retrapi/app.py
@@ -2,10 +2,15 @@ from fastapi import FastAPI
 
 from retrapi import config, database
 from retrapi.handlers.about import handle as handle_about
+from retrapi.handlers.border import handle as handle_border
 from retrapi.handlers.closest import handle as handle_closest
 from retrapi.handlers.find import handle as handle_find
 from retrapi.models.api.request import ClosestRequest, FindRequest
-from retrapi.models.api.response import ClosestResponse, FindResponse
+from retrapi.models.api.response import (
+    BorderResponse,
+    ClosestResponse,
+    FindResponse,
+)
 
 app = FastAPI(title=config.app.title, description=config.app.description)
 
@@ -23,6 +28,11 @@ async def shutdown():
 @app.get("/about", response_model=str)
 async def about():
     return await handle_about()
+
+
+@app.get("/border", response_model=BorderResponse)
+async def border():
+    return await handle_border()
 
 
 @app.post("/closest", response_model=ClosestResponse)
--- mgr/2/SPDB/retrail/api/retrapi/src/retrapi/models/api/response.py
+++ mgr/2/SPDB/retrail/api/retrapi/src/retrapi/models/api/response.py
@@ -1,6 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 from retrapi.models.data import Path, Point
+
+
+class BorderResponse(BaseModel):
+    border: Path
+
+    @validator("border")
+    def border_closes(cls, border: Path):
+        if border.lines[0].start != border.lines[-1].end:
+            raise ValueError("Border is not closed")
+        return border
 
 
 class ClosestResponse(BaseModel):
