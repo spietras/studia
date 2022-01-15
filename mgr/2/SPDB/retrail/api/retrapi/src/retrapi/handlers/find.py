7758f6e5702f0a8e4902630c54ccaf2ae027c37f
Sebastian Pietras <spietras1998@gmail.com>
2022-01-15 21:28:55 +0100

    Better points ordering (#26)

--- mgr/2/SPDB/retrail/api/retrapi/src/retrapi/handlers/find.py
+++ mgr/2/SPDB/retrail/api/retrapi/src/retrapi/handlers/find.py
@@ -1,9 +1,8 @@
 from functools import reduce
 from typing import List, Mapping, Sequence
 
-import numpy as np
-from scipy.spatial import ConvexHull
-from scipy.spatial.qhull import QhullError
+import networkx as nx
+from scipy.spatial import distance
 from sqlalchemy import func, select, sql
 
 import retrapi.models.data as data_models
@@ -13,48 +12,19 @@ from retrapi.models.api.request import FindRequest
 from retrapi.models.api.response import FindResponse
 
 
-def distance(points: np.ndarray, target: np.ndarray) -> np.ndarray:
-    return np.linalg.norm(points - target, axis=-1)
-
-
-def extreme_point(points: np.ndarray, axis: int = -1) -> int:
-    return points.argmax(0)[axis]
-
-
-def furthest_point(points: np.ndarray) -> int:
-    if len(points) < 3:
-        return len(points) - 1
-    try:
-        convex_hull = ConvexHull(points)
-    except QhullError:
-        return extreme_point(points)
-    convex_points = points[convex_hull.vertices]
-    distances = distance(convex_points, convex_points.mean(0))
-    return convex_hull.vertices[distances.argmax()]
-
-
-def sort_by_distance(points: np.ndarray, starting: int) -> np.ndarray:
-    i = starting
-    mask = np.ones(len(points), dtype=bool)
-    mask[i] = False
-    out = points[i, np.newaxis]
-    while len(points[mask]) > 0:
-        distances = distance(points[mask], points[i])
-        i = mask.nonzero()[0][distances.argmin()]
-        out = np.vstack((out, points[i]))
-        mask[i] = False
-    return out
-
-
 def order_points(
     points: Sequence[data_models.Point],
 ) -> List[data_models.Point]:
-    points = np.array([point.tuple for point in points])
-    first_point = furthest_point(points)
-    ordered_points = sort_by_distance(points, first_point)
-    return [
-        data_models.Point(x=point[0], y=point[1]) for point in ordered_points
-    ]
+    graph = nx.Graph()
+    coords = [point.tuple for point in points]
+    distances = distance.cdist(coords, coords, "euclidean")
+    for i in range(len(distances)):
+        for j in range(i, len(distances)):
+            graph.add_edge(i, j, weight=distances[i, j])
+    order = nx.algorithms.approximation.traveling_salesman_problem(
+        graph, cycle=False
+    )
+    return [points[i] for i in order]
 
 
 def create_find_path_query(
