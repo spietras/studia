507795bdc07fe1930ec30c18e48b1dc8fbb2f979
Sebastian Pietras <01133337@pw.edu.pl>
2022-01-20 03:12:20 +0100

    Fixed tokenization of long texts

--- mgr/2/TASS/term-evolution/preprocessing.py
+++ mgr/2/TASS/term-evolution/preprocessing.py
@@ -1,13 +1,19 @@
 import os
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Sequence, Tuple, TypeVar
 
 import spacy
 from spacy import Language
 from spacy.tokens import Token
 
+T = TypeVar('T')
+
 SPACY_EXCLUDED_PIPELINES = ['parser', 'ner', 'entity_linker', 'entity_ruler',
                             'textcat', 'textcat_multilabel', 'senter',
                             'sentencizer', 'tok2vec', 'transformer']
+
+
+def chunks(seq: Sequence[T], chunksize: int) -> T:
+    return (seq[pos:pos + chunksize] for pos in range(0, len(seq), chunksize))
 
 
 def load_spacy(model: str) -> Language:
@@ -32,14 +38,30 @@ def is_irrelevant(token: Token) -> bool:
            token.like_email
 
 
+def tokenize_str(
+        nlp: Language,
+        text: str,
+        chunksize: int = 20000
+) -> List[str]:
+    old_max_length, nlp.max_length = nlp.max_length, chunksize + 1
+    tokens = [
+        token.lemma_
+        for doc in nlp.pipe(chunks(text, chunksize), n_process=os.cpu_count())
+        for token in doc
+        if not is_irrelevant(token)
+    ]
+    nlp.max_length = old_max_length
+    return tokens
+
+
 def tokenize(
         texts: Iterable[str],
-        model: str = 'en_core_web_sm'
+        model: str = 'en_core_web_sm',
+        chunksize: int = 20000
 ) -> Tuple[List[List[str]], List[str]]:
     nlp = load_spacy(model)
-    nlp.max_length = max(len(text) for text in texts) + 1
     tokens = [
-        [token.lemma_ for token in doc if not is_irrelevant(token)]
-        for doc in nlp.pipe(texts, n_process=os.cpu_count())
+        tokenize_str(nlp, text, chunksize)
+        for text in texts
     ]
     return tokens, sorted(set(token for tkns in tokens for token in tkns))
