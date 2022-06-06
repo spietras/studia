d7e91f9bc54a61eb2ce7fce8c1e934acd2450503
Sebastian Pietras <01133337@pw.edu.pl>
2022-06-06 22:33:00 +0200

    Cleaned up

--- mgr/3/ZUM/fraud-detection/src/data_preparation.R
+++ mgr/3/ZUM/fraud-detection/src/data_preparation.R
@@ -12,20 +12,21 @@ build_class_task <- function(data, ...) {
       .keep = "unused"
     )
   }
-  
-  task <- as_task_classif(data, target = "Class", ...)
-  task$col_roles$stratum = "Class"
+
+  task <- as_task_classif(data, target = "Class", positive = "fraud", ...)
+  task$col_roles$stratum <- "Class"
   task
 }
 
 scaling_po <- function(scaling) {
-  if (scaling == "minmax")
-    po <- po("scalerange", param_vals = list(lower = 0, upper = 1))
-  else if (scaling == "zscore")
+  if (scaling == "minmax") {
+    po <- po("scalerange")
+  } else if (scaling == "zscore") {
     po <- po("scale")
-  else if (scaling == "robust")
-    po <- po("scale", param_vals = list(robust = TRUE))
-  
+  } else if (scaling == "robust") {
+    po <- po("scale", robust = TRUE, id = "robustscale")
+  }
+
   po
 }
 
@@ -35,23 +36,21 @@ sampling_po <- function(sampling,
                         oversampling_rate = NULL,
                         smote_rate = NULL,
                         smote_nn = NULL) {
-  
-  
   if (sampling == "undersampling") {
     if (is.null(smote_nn)) {
       stop("undersampling_rate parameter must be filled if undersampling is choosen")
     }
     po <- po("classbalancing",
-             id = "undersample", adjust = "major",
-             reference = "major", shuffle = FALSE, ratio = undersampling_rate
+      id = "undersample", adjust = "major",
+      reference = "major", shuffle = FALSE, ratio = undersampling_rate
     )
   } else if (sampling == "oversampling") {
     if (is.null(smote_nn)) {
       stop("oversampling_rate parameter must be filled if oversampling is choosen")
     }
     po <- po("classbalancing",
-             id = "oversample", adjust = "minor",
-             reference = "minor", shuffle = FALSE, ratio = oversampling_rate
+      id = "oversample", adjust = "minor",
+      reference = "minor", shuffle = FALSE, ratio = oversampling_rate
     )
   } else if (sampling == "SMOTE") {
     if (is.null(smote_nn)) {
@@ -61,7 +60,7 @@ sampling_po <- function(sampling,
   } else {
     stop("Sampling method " + sampling + " unsupported.")
   }
-  
+
   po
 }
 
@@ -80,7 +79,7 @@ cost_matrix <- function(data) {
   frequencies <- data %>%
     count(Class) %>%
     column_to_rownames("Class")
-  costs <- matrix(c(0, 1, frequencies["legit", "n"] / frequencies["fraud", "n"], 0), nrow = 2)
+  costs <- matrix(c(0, 1, frequencies["fraud", "n"] / frequencies["legit", "n"], 0), nrow = 2)
   dimnames(costs) <- list(predicted = c("fraud", "legit"), real = c("fraud", "legit"))
   costs
 }
--- mgr/3/ZUM/fraud-detection/src/helpers.R
+++ mgr/3/ZUM/fraud-detection/src/helpers.R
@@ -26,55 +26,78 @@ tunethreshold <- function(learner,
   )
 }
 
-tunehyperparams <- function(resampling = rsmp("cv", folds = 3),
-                            terminator = trm("stagnation"),
-                            tuner = tnr("random_search"),
-                            ...) {
-  AutoTuner$new(
-    resampling = resampling,
-    terminator = terminator,
-    tuner = tuner,
-    ...
-  )
-}
-
 plot_in_tab <- function(header, plot, level) {
   cat(level, header, "\n")
   suppressMessages(print(plot))
   cat("\n\n")
 }
 
-plot_features_in_tabs <- function(task, level = "####") {
+plot_features_in_tabs <- function(task, level = "###") {
   for (feature in task$feature_names) {
     plot_in_tab(feature, autoplot(task$clone()$select(c(feature)), type = "pairs"), level)
   }
 }
 
-plot_tasks_in_tabs <- function(benchmark, measure, autolimit = TRUE, level = "####", ...) {
-  scores <- bmr$score(measure)[[measure$id]]
-  for (task in benchmark$tasks$task) {
+get_base_learners <- function(names) {
+  suppressWarnings(paste(word(names, -1, sep = fixed("."))))
+}
+
+get_modifiers <- function(names) {
+  suppressWarnings(paste(word(names, 1, -2, sep = fixed("."))))
+}
+
+plot_benchmark_in_tabs <- function(benchmark, grouping, id_modifier, measure, autolimit = TRUE, level = "####", ...) {
+  scores <- benchmark$score(measure)[[measure$id]]
+
+  results <- copy(benchmark$learners)
+  results$group <- grouping(results$learner_id)
+
+  for (group in split(results, results$group)) {
+    bmr <- benchmark$clone(deep = TRUE)$filter(learner_hash = group$learner_hash)
+
+    learners <- copy(bmr$data$learners(states = FALSE, reassemble = FALSE))
+
+    original_ids <- c()
+
+    for (learner in learners$learner) {
+      original_ids <- c(original_ids, learner$id)
+      learner$id <- id_modifier(learner$id)
+    }
+
     plot <- autoplot(
-      benchmark$clone(deep = TRUE)$filter(task_hash = task$hash),
+      BenchmarkResult$new(bmr$data),
       measure = measure,
       ...
-    )
+    ) + theme(plot.margin = margin(10, 10, 10, 50))
     if (autolimit) {
       plot <- plot + ylim(0.9 * min(scores), 1.1 * max(scores))
     }
-    plot_in_tab(task$id, plot, level)
+    plot_in_tab(group$group[[1]], plot, level)
+
+    i <- 1
+    for (learner in learners$learner) {
+      learner$id <- original_ids[[i]]
+      i <- i + 1
+    }
   }
 }
 
+plot_modifiers_in_tabs <- function(benchmark, ...) {
+  plot_benchmark_in_tabs(benchmark, get_modifiers, get_base_learners, ...)
+}
+
+plot_learners_in_tabs <- function(benchmark, ...) {
+  plot_benchmark_in_tabs(benchmark, get_base_learners, get_modifiers, ...)
+}
 
 aggregate_benchmark <- function(benchmark, measure, descending = TRUE) {
   scores <- bmr$aggregate(measure)
   if (descending) scores[order(-scores[[measure$id]])] else scores[order(scores[[measure$id]])]
 }
 
-plot_box <- function(aggregated, metric='classif.costs' ){
-  
-  learners <- c('randomforest', 'knn', 'svm', 'tree')
-  shorten_name <- function(name){
+plot_box <- function(aggregated, metric = "classif.costs") {
+  learners <- c("randomforest", "knn", "svm", "tree")
+  shorten_name <- function(name) {
     name <- str_replace(name, "variable_importance", "vi")
     name <- str_replace(name, "_none_fs", "")
     name <- str_replace(name, "_oversampling", "over_sam")
@@ -82,22 +105,27 @@ plot_box <- function(aggregated, metric='classif.costs' ){
     name <- str_replace(name, "_undersampling", "under_sam")
     name
   }
-  
-  selected <- select(aggregated, learner_id, task_id, m=metric)
-  
-  for (l in learners){
-    s <- filter(selected, learner_id==l)
+
+  selected <- select(aggregated, learner_id, task_id, m = metric)
+
+  for (l in learners) {
+    s <- filter(selected, learner_id == l)
     s <- select(s, task_id, m)
-    s <- mutate(s, task_id, normalization=str_extract(task_id, "zscore|minmax|robust"), task=shorten_name(str_extract(task_id, "_.*$")))
-    if(metric!='classif.costs') 
-      print(ggplot(s, aes(fill=normalization, y=m, x=task)) 
-            + geom_bar(position="dodge", stat="identity") + ggtitle(l)
-            + scale_y_continuous(limits=c(0.95,1),oob = rescale_none) + ylab(metric)
-      )
-    else 
-      print(ggplot(s, aes(fill=normalization, y=m, x=task)) 
-            + geom_bar(position="dodge", stat="identity") + ggtitle(l) + ylab(metric)
-      )
-    
+    s <- mutate(s, task_id, normalization = str_extract(task_id, "zscore|minmax|robust"), task = shorten_name(str_extract(task_id, "_.*$")))
+    if (metric != "classif.costs") {
+      print(ggplot(s, aes(fill = normalization, y = m, x = task))
+      +
+        geom_bar(position = "dodge", stat = "identity") +
+        ggtitle(l)
+        +
+        scale_y_continuous(limits = c(0.95, 1), oob = rescale_none) +
+        ylab(metric))
+    } else {
+      print(ggplot(s, aes(fill = normalization, y = m, x = task))
+      +
+        geom_bar(position = "dodge", stat = "identity") +
+        ggtitle(l) +
+        ylab(metric))
+    }
   }
 }
